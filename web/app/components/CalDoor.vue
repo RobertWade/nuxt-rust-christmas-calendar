b3c53a0cdd826016ae394b596eb77a9180c1e644
Robert Wade <hello@robert-wade.de>
2025-10-30 08:08:26 +0100

    feat(caldoor): create caldoor component

---
+++ web/app/components/CalDoor.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+defineProps({
+    active:{
+        type: Boolean,
+        default: false
+    },
+    title:{
+        type: Number,
+        required: true
+    },
+    icon:{
+        type: String,
+        default: ''
+    },
+    disabled:{
+        type: Boolean,
+        default: false
+    },
+})
+</script>
+
+<template>
+    <NuxtLink to="/door-open"
+        class="relative p-4 border text-xl border-gray-300 rounded-xl flex justify-center items-center aspect-square"
+        :class="active ? 'border-primary' : null">
+        <span v-if="disabled" class="absolute left-1 top-1">🔒</span>
+        {{ title }}
+    </NuxtLink>
+</template>
--- web/app/pages/view-calendar.vue
+++ web/app/pages/view-calendar.vue
@@ -9,15 +9,8 @@ const currentDoor = 3;
             <!-- Content for creating presents goes here -->
             <p>This is the view calendar page.</p>
         </section>
-        <NuxtLink to="/door-open" class="grid grid-cols-4 gap-2">
-            <div 
-            v-for="i in 24" 
-            :key="i"
-            class="relative p-4 border text-xl border-gray-300 rounded-xl flex justify-center items-center aspect-square"
-            :class="currentDoor === i ? 'border-primary' : null"
-            >
-            <span v-if="i>currentDoor" class="absolute left-1 top-1">🔒</span>
-            {{ i }}</div>
-        </NuxtLink>
+        <div class="grid grid-cols-4 gap-4">
+            <CalDoor v-for="i in 24" :key="i" :title="i" :active="currentDoor === i" :disabled="i > currentDoor" />
+        </div>
    </CalPageGrid>
 </template>
